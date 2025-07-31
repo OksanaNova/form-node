e1891a6126d6719a7f7d1d2f11af6c3d3a280566
OksanaNova <o.nova0804@gmail.com>
2025-07-31 14:07:23 -0600

    schema

--- server.js
+++ server.js
@@ -3,7 +3,19 @@ const app = express();
 const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb+srv://oksana:VrIEzPYhTK9ti2DA@cluster0.fyb72nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://oksana:VrIEzPYhTK9ti2DA@cluster0.fyb72nt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+const messageSchema = {
+    name: String,
+    email: String,
+    message: String
+}
+
+const message = mongoose.model('MyMessages', messageSchema);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000')
